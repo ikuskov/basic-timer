6ddcb874c704fb335e840ddd50e28c14ddd5e691
Ivan Kuskov <ivank@fb.com>
2016-11-06 13:49:27 -0800

    First commit

--- BasicTimer/MutliSetExcerciseViewController.swift
+++ BasicTimer/MutliSetExcerciseViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MutliSetExcerciseViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
---
+++ MultiSetExcercise.swift
@@ -0,0 +1,13 @@
+//
+//  MultiSetExcercise.swift
+//  BasicTimer
+//
+//  Created by Ivan Kuskov on 11/6/16.
+//  Copyright © 2016 Ivan Kuskov. All rights reserved.
+//
+
+import Foundation
+
+class MultiSetExcercise {
+  
+}
