44ea5b75f4a8934af3b3d07f9ad69b70173019ff
Ivan Kuskov <i.kuskov@gmail.com>
2017-04-09 13:29:33 -0700

    [TimedSet] Schema updates
    Update time* types Int -> TimeInterval
    Add Schema versioning to successfully load old data
    Note: data won't be encoded properly unless saved explicitly

--- BasicTimer/TimedSet.swift
+++ BasicTimer/TimedSet.swift
@@ -10,25 +10,27 @@ import Foundation
 
 class TimedSet: NSObject, NSCoding, NSCopying {
   
+  // MARK: NSCoding consts
   static let DocumentsDerictory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
   static let ArchiveURL = DocumentsDerictory.appendingPathComponent("routines")
+  static let SchemaVersion = "2"
   
   var name: String = ""
   var repsCount: Int
   var weight: Int
-  var duration: Int = 0
-  var timeStarted: Int = -1
-  var timeStopped: Int = -1
-  var timeComplete: Int = -1
-  var timeLeft: Int = 0
+  var duration: TimeInterval = 0
+  var timeStarted: TimeInterval = -1
+  var timeStopped: TimeInterval = -1
+  var timeComplete: TimeInterval = -1
+  var timeLeft: TimeInterval = 0
   var isRunning: Bool = false
   
-  init?(name: String, repsCount: Int?, weight: Int, duration: Int) {
+  init?(name: String, repsCount: Int?, weight: Int, duration: TimeInterval) {
     self.name = name
     self.repsCount = repsCount ?? 1
     self.weight = weight
     self.duration = duration
-    self.timeLeft = duration
+    self.timeLeft = Double(duration)
     
     super.init()
   }
@@ -63,6 +65,9 @@ class TimedSet: NSObject, NSCoding, NSCopying {
   // MARK: Types
   
   struct Property {
+    /* Special propery to not get NSExceptions on Schema changes */
+    static let schemaVersion = "timed_set_schema_version"
+    
     static let name = "name"
     static let repsCount = "reps"
     static let weight = "weight"
@@ -75,6 +80,9 @@ class TimedSet: NSObject, NSCoding, NSCopying {
   // MARK: NSCoding
   
   func encode(with aCoder: NSCoder) {
+    print("encoding version:", TimedSet.SchemaVersion)
+    aCoder.encode(TimedSet.SchemaVersion, forKey: Property.schemaVersion)
+    
     aCoder.encode(name, forKey: Property.name)
     aCoder.encode(repsCount, forKey: Property.repsCount)
     aCoder.encode(weight, forKey: Property.weight)
@@ -85,13 +93,38 @@ class TimedSet: NSObject, NSCoding, NSCopying {
   }
   
   required convenience init?(coder aDecoder: NSCoder) {
+    var version = aDecoder.decodeObject(forKey: Property.schemaVersion) as? String
+    version = version ?? "Undefined"
+    if version != TimedSet.SchemaVersion {
+      self.init(coder: aDecoder, version: version!)
+      return
+    }
     let name = aDecoder.decodeObject(forKey: Property.name) as! String
     let repsCount = aDecoder.decodeInteger(forKey: Property.repsCount)
     let weight = aDecoder.decodeInteger(forKey: Property.weight)
-    let duration = aDecoder.decodeInteger(forKey: Property.duration)
-    let timeStarted = aDecoder.decodeInteger(forKey: Property.timeStarted) 
-    let timeStopped = aDecoder.decodeInteger(forKey: Property.timeStropped)
-    let timeComplete = aDecoder.decodeInteger(forKey: Property.timeComplete)
+    let duration = aDecoder.decodeDouble(forKey: Property.duration)
+    let timeStarted = aDecoder.decodeDouble(forKey: Property.timeStarted)
+    let timeStopped = aDecoder.decodeDouble(forKey: Property.timeStropped)
+    let timeComplete = aDecoder.decodeDouble(forKey: Property.timeComplete)
+    
+    self.init(name: name, repsCount: repsCount, weight: weight, duration: TimeInterval(duration))
+    self.timeStarted = timeStarted
+    self.timeStopped = timeStopped
+    self.timeComplete = timeComplete
+  }
+  
+  /* For version 1 */
+  convenience init?(coder aDecoder: NSCoder, version: String) {
+    print("Fallback init for version: ", version)
+    let name = aDecoder.decodeObject(forKey: Property.name) as! String
+    let repsCount = aDecoder.decodeInteger(forKey: Property.repsCount)
+    let weight = aDecoder.decodeInteger(forKey: Property.weight)
+    /* Old version - begin */
+    let duration = TimeInterval(aDecoder.decodeInteger(forKey: Property.duration))
+    let timeStarted = Double(aDecoder.decodeInteger(forKey: Property.timeStarted))
+    let timeStopped = Double(aDecoder.decodeInteger(forKey: Property.timeStropped))
+    let timeComplete = Double(aDecoder.decodeInteger(forKey: Property.timeComplete))
+    /* Old version - end */
     
     self.init(name: name, repsCount: repsCount, weight: weight, duration: duration)
     self.timeStarted = timeStarted
